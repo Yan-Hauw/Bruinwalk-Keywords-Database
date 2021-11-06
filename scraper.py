cd5de039d1c10d90a2d8c39bea0f377cfa9edc3b
Yan-Hauw <91395373+Yan-Hauw@users.noreply.github.com>
2021-11-06 16:34:14 -0700

    Improved to scrape entire pagination for a given class url

--- scraper.py
+++ scraper.py
@@ -2,33 +2,50 @@ import sys
 import re
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
 
 browser = webdriver.Firefox()
 
 
-browser.get("https://www.bruinwalk.com/professors/junghoo-cho/com-sci-143/")
+browser.get("https://www.bruinwalk.com/professors/parvaneh-ghaforyfrd/com-sci-33/")
 
-html_file = browser.page_source
-
-soup = BeautifulSoup(html_file, "html.parser")
-
-keywords = {"slides", "textbook"}
+text = ""
 
 dict = {}
 
+while True:
 
-text = soup.get_text()
+    if text:
+        next_button = browser.find_element(
+            By.XPATH, "//div[@class='paginator']/span/a[2]"
+        )
+        if "disabled" in next_button.get_attribute("class"):
+            print("break")
+            break
+        else:
+            print("click")
+            next_button.click()
 
-text = re.sub(r"[^a-zA-Z ]", "", text)
+    html_file = browser.page_source
 
-words = text.split()
+    soup = BeautifulSoup(html_file, "html.parser")
 
-for w in words:
-    for i in keywords:
-        if i in w:
-            dict[i] = dict[i] + 1 if i in dict else 1
+    keywords = {"slides", "textbook"}
+
+    text = soup.get_text()
+
+    text = re.sub(r"[^a-zA-Z ]", "", text)
+
+    words = text.split()
+
+    for w in words:
+        for i in keywords:
+            if i in w:
+                dict[i] = dict[i] + 1 if i in dict else 1
+
+    print("success")
 
 
 print(dict)
