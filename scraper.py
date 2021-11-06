b96ccba3758409e3f14604c2c3f3523639c1fcd5
Yan-Hauw <91395373+Yan-Hauw@users.noreply.github.com>
2021-11-06 13:59:59 -0700

    put no. of occurrences of each keyword into dictionary

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -13,13 +14,23 @@ html_file = browser.page_source
 
 soup = BeautifulSoup(html_file, "html.parser")
 
+keywords = {"slides", "textbook"}
 
-# text is a string
+dict = {}
+
+
 text = soup.get_text()
+
+text = re.sub(r"[^a-zA-Z ]", "", text)
 
 words = text.split()
 
+for w in words:
+    for i in keywords:
+        if i in w:
+            dict[i] = dict[i] + 1 if i in dict else 1
 
-print(words)
+
+print(dict)
 
 browser.close()
