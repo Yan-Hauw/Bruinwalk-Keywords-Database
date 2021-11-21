c9b36ad9168696362934803c6c497a9b0a70319b
Yan-Hauw <91395373+Yan-Hauw@users.noreply.github.com>
2021-11-20 17:35:29 -0800

    put code for scraping data for each lecturer of a course into separate file

--- bruinwalkautoscraper/classscraper.py
+++ bruinwalkautoscraper/classscraper.py
@@ -1,7 +1,7 @@
 # other modules
+from bruinwalkautoscraper.get_class_data import get_class_data
 from enter_course import enter_course
 
-from pagescraper import scrape_page
 
 # constants
 from constants.dept_keywords import dept_identifiers
@@ -11,10 +11,6 @@ from utils.utils import string_to_number
 
 # libraries
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
 
 
 dept_input_text = "Linguistics"
@@ -30,34 +26,7 @@ browser = enter_course(
     browser, dept_input_text, dept_keyword, classsearch_input_text, class_number
 )
 
-class_data = []
-
-while True:
-
-    course_professor_anchors = browser.find_elements(By.XPATH, "//tr/*[2]/a")
-
-    urls = []
-
-    for a in course_professor_anchors:
-        urls.append(a.get_attribute("href"))
-
-    professor_spans = browser.find_elements(By.XPATH, "//a/*[1][@class='prof name']")
-
-    for span in professor_spans:
-        url = urls.pop(0)
-        results = scrape_page(url)
-        res_tuple = (dept_keyword + class_number, span.text, results)
-        class_data.append(res_tuple)
-
-    next_button = browser.find_element(By.XPATH, "//div[@class='paginator']/span/a[2]")
-    if "disabled" in next_button.get_attribute("class"):
-        break
-    else:
-        next_button.click()
-
-print(len(class_data))
-
-print(class_data)
+class_data = get_class_data(browser, dept_keyword, class_number)
 
 
 browser.close()
---
+++ bruinwalkautoscraper/get_class_data.py
@@ -0,0 +1,38 @@
+# other modules
+from pagescraper import scrape_page
+
+# libraries
+from selenium.webdriver.common.by import By
+
+
+def get_class_data(browser, dept_keyword, class_number):
+    class_data = []
+
+    while True:
+
+        course_professor_anchors = browser.find_elements(By.XPATH, "//tr/*[2]/a")
+
+        urls = []
+
+        for a in course_professor_anchors:
+            urls.append(a.get_attribute("href"))
+
+        professor_spans = browser.find_elements(
+            By.XPATH, "//a/*[1][@class='prof name']"
+        )
+
+        for span in professor_spans:
+            url = urls.pop(0)
+            results = scrape_page(url)
+            res_tuple = (dept_keyword + class_number, span.text, results)
+            class_data.append(res_tuple)
+
+        next_button = browser.find_element(
+            By.XPATH, "//div[@class='paginator']/span/a[2]"
+        )
+        if "disabled" in next_button.get_attribute("class"):
+            break
+        else:
+            next_button.click()
+
+    return class_data
