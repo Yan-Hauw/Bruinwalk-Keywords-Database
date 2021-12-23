f487d720a5487e1e395e9d7203fe4088eabf487a
Yan-Hauw <91395373+Yan-Hauw@users.noreply.github.com>
2021-12-22 18:04:38 -0800

    installed dependencies, got loading into mysql working

--- bruinwalkautoscraper/load_table.py
+++ bruinwalkautoscraper/load_table.py
@@ -1,16 +1,37 @@
-import mysql.connector
-from mysql.connector import errorcode
+import csv
+import pandas as pd
+from sqlalchemy import create_engine
 
 # constants
 from constants.server_keys import mysqlServer
+from constants.scraping_keywords import scraping_keywords
 
-cnx = mysql.connector.connect(
-    user=mysqlServer.username, password=mysqlServer.password, database="departments"
-)
-cursor = cnx.cursor()
+engine_path = "mysql://{username}:{password}@{host}/{db_name}"
 
-cursor.execute()
+keys_template = {
+    "username": mysqlServer.username,
+    "password": mysqlServer.password,
+    "host": mysqlServer.host,
+    "db_name": mysqlServer.database,
+}
 
+engine = create_engine(
+    engine_path.format(**keys_template)
+)  # enter your password and database names here
 
-cursor.close()
-cnx.close()
+keyword_cols = list(scraping_keywords)
+
+cols = ["course_name", "lecturer"]
+
+cols += keyword_cols
+
+df = pd.read_csv(
+    "scraper_data.csv",
+    sep=",",
+    quotechar='"',
+    header=None,
+    names=cols,
+)  # Replace Excel_file_name with your excel sheet name
+df.to_sql(
+    "cs", con=engine, index=False, if_exists="append"
+)  # Replace Table_name with your sql table name
